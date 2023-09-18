bd8b6acc730dff66ee2dd3d2a8334f4d3485af95
arihant12 <arihant@antiersolutions.com>
2023-09-18 13:28:38 +0530

    block added with button

--- src/AddMore.js
+++ src/AddMore.js
@@ -5,7 +5,7 @@ const AddMore = () => {
   const [blocksAdded, setBlocksAdded] = useState(0);
 
   const handleAddBlockClick = () => {
-    if (blocksAdded < 2) {
+    if (blocksAdded < 3) {
       setBlocksAdded(blocksAdded + 1);
     }
   };
@@ -17,13 +17,21 @@ const AddMore = () => {
           <h2>Block D</h2>
         </div>
       </div>
-      {blocksAdded < 2 && <div>Block after 'd'</div>}
-      <div className="block-container">
-        <div className="block">
-          <h2>Block E</h2>
+      {blocksAdded >= 1 && (
+        <div className="block-container">
+          <div className="block">
+            <h2>Block E</h2>
+          </div>
         </div>
-      </div>{" "}
-      {blocksAdded < 2 && (
+      )}
+      {blocksAdded >= 2 && (
+        <div className="block-container">
+          <div className="block">
+            <h2>Block F</h2>
+          </div>
+        </div>
+      )}
+      {blocksAdded < 3 && blocksAdded < 2 && (
         <button onClick={handleAddBlockClick}>Add Block</button>
       )}
     </div>
